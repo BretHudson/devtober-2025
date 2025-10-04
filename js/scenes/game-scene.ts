f1f1e3aa8ef30634c384b2609748ab593c0614d0
Bret Hudson <bret@brethudson.com>
2025-10-03 19:34:13 -0700

    Refactor updateCamera code

--- js/scenes/game-scene.ts
+++ js/scenes/game-scene.ts
@@ -1,5 +1,6 @@
 import type { Entity } from 'canvas-lord/core/entity';
 import { Scene } from 'canvas-lord/core/scene';
+import { Vec2 } from 'canvas-lord/math';
 import type { Player } from '~/entities/player';
 
 export class GameScene extends Scene {
@@ -17,21 +18,24 @@ export class GameScene extends Scene {
 	}
 
 	updateCamera() {
+		const { cameraTarget } = this;
+
+		if (!cameraTarget) return;
+
 		let newX = this.camera.x;
 		let newY = this.camera.y;
-		if (this.cameraTarget) {
-			let target = this.cameraTarget.pos;
 
-			// TODO(bret): this is so hacked together lmao
-			if (this.cameraTarget && this.cameraTarget === this.player) {
-				const offset = this.player.aim.sub(this.player.pos).scale(0.1);
-				offset.y *= 16 / 9;
-				target = target.add(offset);
-			}
-
-			newX = target.x;
-			newY = target.y;
+		let offset = Vec2.zero;
+		// TODO(bret): this is so hacked together lmao
+		if (cameraTarget === this.player) {
+			offset = this.player.aim.sub(this.player.pos).scale(0.1);
+			offset.y *= 16 / 9;
 		}
+
+		const target = cameraTarget.pos.add(offset);
+
+		newX = target.x;
+		newY = target.y;
 
 		if (this.bounds) {
 			newX = Math.clamp(newX, this.bounds[0], this.bounds[2]);
